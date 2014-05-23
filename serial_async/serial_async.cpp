4451904e6d55eabcd43ee40378bcca946c7b4549
Kohei Izumi <kohei.izumi@gmail.com>
2014-05-23 21:10:32 +0900

    非同期通信で受信と送信を出来るようにした（mbedで確認済み）

--- serial_async/serial_async.cpp
+++ serial_async/serial_async.cpp
@@ -2,42 +2,18 @@
 #include <boost/bind.hpp>
 #include <boost/thread.hpp>
 #include <boost/array.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 #include <iostream>
-#include <fstream>
 
 using namespace boost::asio;
-using namespace boost::posix_time;
 
-static bool isRunning(true);
 const char *PORT = "COM3";
 io_service io;
 serial_port port( io, PORT );
 boost::array<char, 64> rbuf;
-int fp = 0;
-char fbuf[128] = {0};
-std::ofstream ofs;
-boost::mutex mtx;
-
-static void waitKeyPressed(void)
-{
-	getchar();
-	isRunning = false;
-}
 
 void read_callback(const boost::system::error_code& e, std::size_t size)
 {
-	boost::mutex::scoped_lock lk(mtx);
-
-	for(unsigned int i=0;i<size;i++){
-		char c = rbuf.at(i);
-		fbuf[fp++] = c;
-		if(c == '\n'){
-			ofs.write(fbuf, fp);
-			fp = 0;
-		}
-	}
-
+	std::cout.write(rbuf.data(), size);
 	port.async_read_some( buffer(rbuf), boost::bind(&read_callback, _1, _2 ));
 }
 
@@ -48,13 +24,8 @@ void write_callback(const boost::system::error_code& e, std::size_t size )
 
 int main(int argc, char *argv[])
 {
-	const boost::thread thr_wait(&waitKeyPressed);
-
-	//std::string wbuf = argv[1];
-	std::string wbuf = "on\n";
-	ptime now = second_clock::local_time();
-	std::string logname = to_iso_string(now) + std::string(".log");
-	ofs.open(logname.c_str());
+	std::string wbuf_on = "on\n";
+	std::string wbuf_off = "off\n";
 
 	port.set_option(serial_port_base::baud_rate(9600));
 	port.set_option(serial_port_base::character_size(8));
@@ -64,14 +35,13 @@ int main(int argc, char *argv[])
 
 	boost::thread thr_io(boost::bind(&io_service::run, &io));
 
-	//port.async_read_some( buffer(rbuf), boost::bind(&read_callback, _1, _2 ));
-
-	port.async_write_some( buffer(wbuf), boost::bind(&write_callback, _1, _2));
-
-	port.async_read_some( buffer(rbuf), boost::bind(&read_callback, _1, _2 ));
-	while(isRunning){
-		boost::this_thread::sleep(boost::posix_time::milliseconds(2000));
+	while(1){
+		port.async_read_some( buffer(rbuf), boost::bind(&read_callback, _1, _2 ));
+		port.async_write_some( buffer(wbuf_on), boost::bind(&write_callback, _1, _2));
+		boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+		port.async_read_some( buffer(rbuf), boost::bind(&read_callback, _1, _2 ));
+		port.async_write_some( buffer(wbuf_off), boost::bind(&write_callback, _1, _2));
+		boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
-
 	return 0;
 }
